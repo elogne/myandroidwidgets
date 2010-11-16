52db6f0a194e3c22a3db75d32af247700bb1e0ce
coomar.101 <coomar.101@d5c1a463-17a8-e326-acdc-c018575ee69e>
2010-11-16 20:13:43 +0000

    Stop animation

--- AnimationSample/src/com/beanie/animations/sample1/HomeActivity.java
+++ AnimationSample/src/com/beanie/animations/sample1/HomeActivity.java
@@ -9,22 +9,30 @@ import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 public class HomeActivity extends Activity {
+    private ImageView imageView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final ImageView btn = (ImageView) findViewById(R.id.ImageButton01);
-        btn.setOnClickListener(new OnClickListener() {
+        imageView = (ImageView) findViewById(R.id.ImageButton01);
+        imageView.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                AnimationDrawable animator = (AnimationDrawable) btn.getBackground();
-                btn.setImageDrawable(null);
+                AnimationDrawable animator = (AnimationDrawable) imageView.getBackground();
+                imageView.setImageDrawable(null);
                 animator.start();
             }
         });
 
+    }
+    
+    // Call this method to stop the animation
+    public void stopAnimation(){
+        AnimationDrawable animator = (AnimationDrawable) imageView.getBackground();
+        animator.stop();
+        imageView.setImageResource(R.drawable.icon);
     }
 }
